ddf8ab3ca0e2c462d3d1d2d6b35819a16a735b0f
Krecik <bartoszkraczek2006@gmail.com>
2023-08-25 17:49:08 +0200

    updatye

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 // select header
+const header = document.querySelector('.header')
 // select nav
 // select nav trigger
 
 // open - close menu
 
 // header background on scroll
+window.addEventListener('scroll', () =>{
+    if(window.scrollY > 40){
+        header.classList.add('active');
+    }else{
+        header.classList.remove('active');
+    }
+})
 
 // Initialize Swiper
 
