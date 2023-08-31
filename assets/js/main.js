1b16cd2d9876713632a28d3ee25d904b91a0ad95
Krecik <bartoszkraczek2006@gmail.com>
2023-08-31 13:23:50 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,20 @@ window.addEventListener('scroll', () =>{
     }
 })
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+
+sr.reveal('.hero__title, .features__container, .companies__pretitle, .companies__title, .footer__container');
+sr.reveal('.hero__subtitle', {delay: 500});
+sr.reveal('.hero__btn', {delay: 500});
+sr.reveal('.hero__img', {delay: 600, origin: 'bottom'});
+sr.reveal('.feature', {interval: 100});
+sr.reveal('.section-primary__img', { origin: 'left'});
+sr.reveal('.section-primary__text', { origin: 'right'});
+
+sr.reveal('.brands__item', { interval: '100'});
